96ab6c69bad0e371b581237ea117245f3ac85784
Marek Zelinka <mzelinka17@gmail.com>
2023-02-16 11:59:32 +0100

    update app

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,21 @@
+import { SearchForm } from "./components/search-form";
+
 export function App() {
-  return <div className="app"></div>;
+  const search = (country) => {
+    console.log(`Searching for ${country} info`);
+  };
+
+  return (
+    <div className="app">
+      <main>
+        <h1>Country Info</h1>
+        <h2>Search by name</h2>
+        <SearchForm onSubmit={search} />
+        <h2>Results</h2>
+      </main>
+      <footer>
+        <i>CountryInfo &copy; 2023 Marek Zelinka</i>
+      </footer>
+    </div>
+  );
 }
